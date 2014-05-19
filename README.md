41e284cc1413e9fa413042b7b00679bd72b6af54
skyline75489 <skyline75489@outlook.com>
2014-05-19 19:34:22 +0800

    更新Python脚本，基本完成查询功能

--- README.md
+++ README.md
@@ -10,8 +10,47 @@ PC截图
 ![PC截图](https://github.com/HIT-ON-Github/hitlib2/raw/master/screenshot-pc.png)
 
 移动设备截图(iPod Touch 4, iOS 6.1.5)
+
 ![移动设备截图](https://raw.githubusercontent.com/HIT-ON-Github/hitlib2/master/screenshot-mobile.png)
 
 ###没错，百度，哥抄的就是你！
 
-附带Python脚本一枚(尚未完成)，不想看PHP代码的可以看看Python代码。我知道大家都喜欢Python。
+附带Python脚本一枚，不想看PHP代码的可以看看Python代码。我知道大家都喜欢Python。
+
+
+示例
+
+```python
+>>> from hitlib2 import query
+>>> a = query("网页")
+>>> b = query("读者",qk")
+>>> c = query("软件","lw")
+>>> a
+[{'publisher': u'\u673a\u68b0\u5de5\u4e1a\u51fa\u7248\u793e', 'author': u'\u5218\u745e\u65b0 \
+.......
+>>> from hitlib2 import prettyprint
+>>> prettyprint(a)
+网页设计与制作教程
+网页设计与制作
+Dreamweaver+Flash+Photoshop网页设计从入门到精通
+网页设计与制作
+DIV+CSS 3.0网页布局实战从入门到精通
+网站规划与动态网页设计
+Dreamweaver网页设计与制作
+网站规划与网页设计
+中文版Dreamweaver CS6  Flash CS6  Photoshop CS6网页设计基础培训教程
+巧学巧用Dreamweaver CS6制作网页
+>>> prettyprint(a,"author")
+刘瑞新 张兵义主编 刘瑞新 张兵义
+张国庆主编 张国庆
+李东博编著 李东博
+修毅 洪颖 邵熹雯编著 修毅 洪颖 邵熹雯
+新视角文化行编著
+舒后 何薇编著 舒后 何薇
+祁瑞华主编 祁瑞华
+杜永红主编 杜永红
+何秀芳编著 何秀芳
+刘西杰编著 刘西杰
+>>> 
+
+```
--- hitlib2.py
+++ hitlib2.py
@@ -6,29 +6,97 @@ from BeautifulSoup import BeautifulSoup
 
 BASE_URL = "http://202.118.250.131/lib/opacAction.do?method=DoAjax&dispatch=smcx&type=smcx&method1=1&retrieveLib=1"
 
+#书目
 SM_TYPE="589"
+#期刊
 QK_TYPE="590"
+#论文
 LW_TYPE="666"
 
-def query(title ,type="sm") :
+def _requset(title,query_type,page):
+	response = None
+	try:
+		response = requests.get(BASE_URL + "&title=" + title + "&pabookType=" + query_type + "&smcx_p=" + str(page));
+	except Exception as e:
+		pass
+	if(response):
+		return BeautifulSoup(response.content)
+	else:
+		raise TypeError("Response is None")
 
-	response = requests.get(BASE_URL+"&title=" + title + "&pabookType=" + SM_TYPE);
-	html = BeautifulSoup(response.content)
-	trs = html.findAll(onmouseover=True)
-	booklist = []
-	for tr in trs:
-		bookinfo = []
-		detail = tr.find(onclick=True)['onclick']
-		for td in tr.findAll("td"):
-			text = td.text.replace("/","")
-			text = text.replace("=","")
-			text = text.replace("\x1e"," ")
-			bookinfo.append(text)
-		bookinfo.append(detail)
-		booklist.append(bookinfo)
-
-	return booklist
+def _digest_info(tr):
+	info = []
+	id_reg = re.compile(r'showDetail\(\'(\d*)\'\,')
+	id_text = tr.find(onclick=True)['onclick']
+	for td in tr.findAll("td"):
+		text = td.text.replace("/","")
+		text = text.replace("=","")
+		text = text.replace("\x1e"," ")
+		info.append(text)
 		
+	info.append(int(id_reg.findall(id_text)[0]))
+	return info
 
+def _make_sm_dict(info):
+	info_dict = {}
+	info_dict['title'] = info[0]
+	info_dict['author'] = info[1]
+	info_dict['publisher'] = info[2]
+	info_dict['year'] = info[3]
+	info_dict['pages'] = info[4]
+	info_dict['sn'] = info[5]
+	info_dict['id'] = info[7]
+	return info_dict
 
-#query("运筹学")
+def _make_qk_dict(info):
+	info_dict = {}
+	info_dict['title'] = info[0]
+	info_dict['enterprise'] = info[1]
+	info_dict['publisher'] = info[2]
+	info_dict['sn'] = info[5]
+	info_dict['id'] = info[7]
+	return info_dict
+
+def _make_lw_dict(info):
+	info_dict = {}
+	info_dict['title'] = info[0]
+	info_dict['author'] = info[1]
+	info_dict['year'] = info[3]
+	info_dict['sn'] = info[5]
+	info_dict['id'] = info[7]
+	return info_dict
+
+def _resolve_html(html,query_type):
+	trs = html.findAll(onmouseover=True)
+	result_list = []
+	for tr in trs:
+		info = _digest_info(tr)
+		row_dict = {}
+		if query_type == SM_TYPE:
+			row_dict = _make_sm_dict(info)
+		if query_type == QK_TYPE:
+			row_dict = _make_qk_dict(info)
+		if query_type == LW_TYPE:
+			row_dict = _make_lw_dict(info)
+
+		result_list.append(row_dict)
+	return result_list
+
+def query(title, q_type="sm", page=1) :
+	query_type = SM_TYPE
+	if q_type == "qk":
+		query_type == QK_TYPE
+	elif q_type == "lw":
+		query_type == LW_TYPE
+	
+	html = _requset(title,query_type,page)
+	result = []
+	result = _resolve_html(html,query_type)
+	return result
+
+def prettyprint(result_list,keyword="title"):
+	for a in result_list:
+		if isinstance(a,dict):
+			print(a[keyword])
+		else:
+			raise TypeError("Non-dict type found in list")
